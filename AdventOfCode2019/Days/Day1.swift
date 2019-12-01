775e1480a222dc7ab27973ebf658a7dc86a99136
Bastian Stien <bastian.stien@gmail.com>
2019-12-01 12:44:40 +0100

    Day 1 - part 2

--- AdventOfCode2019/Days/Day1.swift
+++ AdventOfCode2019/Days/Day1.swift
@@ -2,13 +2,32 @@ class Day1: Day {
     static func run(input: String) {
         let lines = splitIntoLines(input: input)
         let part1 = part1FuelRequirements(lines: lines)
+        let part2 = part2FuelRequirementsForFuel(lines: lines)
 
         print("Fuel requirements for modules, part 1: \(part1)")
+        print("Fuel requirements for modules, including fuel mass, part 2: \(part2)")
     }
 
     static func part1FuelRequirements(lines: [String]) -> Double {
         lines.compactMap(Double.init)
-             .map { ($0 / 3.0).rounded(.down) - 2 }
+             .map(fuelRequiredForMass)
              .reduce(into: 0, { $0 += $1 })
+    }
+
+    static func part2FuelRequirementsForFuel(lines: [String]) -> Double {
+        lines.compactMap(Double.init)
+             .map(recursiveFuelRequiredForMass)
+             .reduce(into: 0, { $0 += $1 })
+    }
+
+    static func fuelRequiredForMass(mass: Double) -> Double {
+        (mass / 3.0).rounded(.down) - 2
+    }
+
+    static func recursiveFuelRequiredForMass(mass: Double) -> Double {
+        let fuelRequired = fuelRequiredForMass(mass: mass)
+        guard fuelRequired > 0 else { return 0 }
+
+        return fuelRequired + recursiveFuelRequiredForMass(mass: fuelRequired)
     }
 }
