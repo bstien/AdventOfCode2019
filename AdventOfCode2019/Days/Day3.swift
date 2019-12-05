7a49814cf606ff51fa9f3fd7a2ce7fedcb46db0a
Bastian Stien <bastian.stien@gmail.com>
2019-12-05 21:32:36 +0100

    Day 3 - part 1

--- AdventOfCode2019/Days/Day3.swift
+++ AdventOfCode2019/Days/Day3.swift
@@ -1,29 +1,59 @@
 class Day3: Day {
     static func run(input: String) {
-        let input = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
-
         let cables = splitInput(input)
             .map { splitInput($0, separator: ",") }
             .map { $0.compactMap(CableSegment.init) }
+            .map { Cable(segments: $0) }
 
         let part1 = part1ManhattanDistanceToNearestIntersection(using: cables)
 
         print("Manhattan distance to nearest intersection, part 1: \(part1)")
     }
 
-    static private func part1ManhattanDistanceToNearestIntersection(using cables: [[CableSegment]]) -> Int {
-        for cable in cables {
-            for segment in cable {
+    static private func part1ManhattanDistanceToNearestIntersection(using cables: [Cable]) -> Int {
+        let cableA = traverseCable(cable: cables[0])
+        let cableB = traverseCable(cable: cables[1])
+        return cableA.intersection(cableB).map({ $0.distance }).min() ?? 0
+    }
 
+    static private func traverseCable(cable: Cable) -> Set<Point> {
+        var x = 0
+        var y = 0
+
+        return Set(cable.segments.flatMap { segment in
+            (0..<segment.length).map { _ in
+                x += 1 * segment.direction.movement.x
+                y += 1 * segment.direction.movement.y
+                return Point(x: x, y: y)
             }
-        }
-        return 0
+        })
     }
 }
 
+private struct Point: Hashable {
+    let x: Int
+    let y: Int
+
+    var distance: Int {
+        abs(x) + abs(y)
+    }
+}
+
+private struct Cable {
+    let segments: [CableSegment]
+}
 
 private enum Direction {
     case up, down, left, right
+
+    var movement: (x: Int, y: Int) {
+        switch self {
+        case .up: return (x: 0, y: -1)
+        case .down: return (x: 0, y: 1)
+        case .right: return (x: 1, y: 0)
+        case .left: return (x: -1, y: 0)
+        }
+    }
 
     init?(rawValue: Character) {
         switch rawValue.lowercased() {
